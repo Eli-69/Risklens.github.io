02337d1145f30df680d80c2bf90f87ce869ab7aa
Eli-69 <Thatonekidej@gmail.com>
2026-03-01 14:40:03 -0600

    Change router to hash build

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router"; // <-- change this line
 import { Home } from "./pages/Home";
 import { SecurityInsights } from "./pages/SecurityInsights";
 import { HowItWorks } from "./pages/HowItWorks";
@@ -8,39 +8,13 @@ import { WhosAtRisk } from "./pages/WhosAtRisk";
 import { ReportSite } from "./pages/ReportSite";
 import { Help } from "./pages/Help";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Home,
-  },
-  {
-    path: "/insights/:domain",
-    Component: SecurityInsights,
-  },
-  {
-    path: "/how-it-works",
-    Component: HowItWorks,
-  },
-  {
-    path: "/login",
-    Component: Login,
-  },
-  {
-    path: "/dashboard",
-    Component: Dashboard,
-  },
-  {
-    path: "/whos-at-risk",
-    Component: WhosAtRisk,
-  },
-  {
-    path: "/report-site",
-    Component: ReportSite,
-  },
-  {
-    path: "/help",
-    Component: Help,
-  },
-], {
-  basename: "/Risklens.github.io"
-});
+export const router = createHashRouter([
+  { path: "/", Component: Home },
+  { path: "/insights/:domain", Component: SecurityInsights },
+  { path: "/how-it-works", Component: HowItWorks },
+  { path: "/login", Component: Login },
+  { path: "/dashboard", Component: Dashboard },
+  { path: "/whos-at-risk", Component: WhosAtRisk },
+  { path: "/report-site", Component: ReportSite },
+  { path: "/help", Component: Help },
+]);
